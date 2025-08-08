ac44db1bbb2009dc0ab4802c87bd7e73aa1f4b15
labizoon <labimirza465@gmail.com>
2025-08-08 18:05:27 +0000

    string to number✍️"

---
+++ js4.js
@@ -0,0 +1,41 @@
+//string to number
+let str="1234";
+let num = Number(str);
+console.log(num); //1234
+console.log(typeof num); //"number"
+
+
+let str1= "3.14";
+let num1 = +str1;
+console.log(num1); //3.14
+console.log(typeof num1); //"number"
+
+let str2 = "123.45";
+let num2 = parseInt(str2);
+console.log(num2); //123
+console.log(typeof num2); //"number"
+
+let str3 = "123.54";
+let num3 = parseFloat(str3);
+console.log(str3); //"123.54"
+console.log(typeof num3); //"number"
+
+//number to string
+let number = 12;
+let string = String(number);
+console.log(string); "12"   
+console.log(typeof string); "string"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
